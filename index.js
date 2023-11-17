93538d95de06d924e2f7f02f5bc90fae4af6422a
TheTaiMan <68861499+TheTaiMan@users.noreply.github.com>
2023-11-17 23:42:38 +0000

    Here you are

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ function info() {
     author: '', // TODO: Your Battlesnake Username
     color: '#888888', // TODO: Choose color
     head: 'default', // TODO: Choose head
-    tail: 'default', // TODO: Choose tail
+    tail: 'default', // TODO: Choose tail88
   }
 }
 function getCurrentTimeCST() {
