1fc36257b894f23e0ddfecd6ea6b7f8a0e3cad84
pieberrykinnie <dmtruong1980@gmail.com>
2025-01-10 15:18:19 -0500

    chore: Add logs to autopush script

--- commit_and_push.sh
+++ commit_and_push.sh
@@ -5,6 +5,25 @@ git config --global user.email "dmtruong1980@gmail.com"
 git config --global user.name "Quoc Hoang Vu"
 
 # Add, commit, and push the new trained weights
+echo "Adding changes..."
 git add saved_model/
+if [ $? -ne 0 ]; then
+  echo "Failed to add changes"
+  exit 1
+fi
+
+echo "Committing changes..."
 git commit -m "Update trained weights"
+if [ $? -ne 0 ]; then
+  echo "Failed to commit changes"
+  exit 1
+fi
+
+echo "Pushing changes..."
 git push
+if [ $? -ne 0 ]; then
+  echo "Failed to push changes"
+  exit 1
+fi
+
+echo "Changes pushed successfully"
