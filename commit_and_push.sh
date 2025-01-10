700d58a22d60ed0d83cfb1a72a2b56fea1135105
pieberrykinnie <dmtruong1980@gmail.com>
2025-01-10 14:34:32 -0500

    fix: Config Git credentials

--- commit_and_push.sh
+++ commit_and_push.sh
@@ -1,3 +1,10 @@
+#!/bin/sh
+
+# Configure Git user information
+git config --global user.email "dmtruong1980@gmail.com"
+git config --global user.name "Quoc Hoang Vu"
+
+# Add, commit, and push the new trained weights
 git add saved_model/
 git commit -m "Update trained weights"
-git push
+git push https://$GITHUB_TOKEN@github.com/your-username/your-repo.git
